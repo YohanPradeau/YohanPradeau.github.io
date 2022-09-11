963df2543e3b9e4490161b12ffb58f693e2b27db
Yohan Pradeau <yoh.pradeau@gmail.com>
2022-09-11 17:16:42 +0200

    ajout realisation + comp trans

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,16 @@
+///PROJECT
+document.getElementById("MCUSYS").onclick = function () { trigger_modal_show("MCUSYS") };
+document.getElementById("NETSYS").onclick = function () { trigger_modal_show("NETSYS") };
+document.getElementById("VSTT").onclick = function () { trigger_modal_show("VSTT") };
+document.getElementById("CHECKEAT").onclick = function () { trigger_modal_show("CHECKEAT") };
+document.getElementById("UPSACALENDAR").onclick = function () { trigger_modal_show("UPSACALENDAR") };
+
+document.getElementById("modal-MCUSYS").onclick = function () { trigger_modal_hide("MCUSYS") };
+document.getElementById("modal-NETSYS").onclick = function () { trigger_modal_hide("NETSYS") };
+document.getElementById("modal-VSTT").onclick = function () { trigger_modal_hide("VSTT") };
+document.getElementById("modal-CHECKEAT").onclick = function () { trigger_modal_hide("CHECKEAT") };
+document.getElementById("modal-UPSACALENDAR").onclick = function () { trigger_modal_hide("UPSACALENDAR") };
+
 //TECH
 document.getElementById("JS").onclick = function () { trigger_modal_show("JS") };
 document.getElementById("PHP").onclick = function () { trigger_modal_show("PHP") };
@@ -15,8 +28,14 @@ document.getElementById("modal-CS").onclick = function () { trigger_modal_hide("
 
 //TRANS
 document.getElementById("FORMATIONS").onclick = function () { trigger_modal_show("FORMATIONS") };
+document.getElementById("AS").onclick = function () { trigger_modal_show("AS") };
+document.getElementById("VT").onclick = function () { trigger_modal_show("VT") };
+document.getElementById("POLY").onclick = function () { trigger_modal_show("POLY") };
 
 document.getElementById("modal-FORMATIONS").onclick = function () { trigger_modal_hide("FORMATIONS") };
+document.getElementById("modal-AS").onclick = function () { trigger_modal_hide("AS") };
+document.getElementById("modal-VT").onclick = function () { trigger_modal_hide("VT") };
+document.getElementById("modal-POLY").onclick = function () { trigger_modal_hide("POLY") };
 
 
 
